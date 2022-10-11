577694d75167a8b19bc76a51a601a482fead1851
BadMickey <joaovitorcampos161@gmail.com>
2022-10-11 13:51:48 -0300

    Lista unicamente encadeada feita

--- ListaUnicamenteUnificada/linkedlist.py
+++ ListaUnicamenteUnificada/linkedlist.py
@@ -20,20 +20,65 @@ class LinkedList(object):
 
     def __len__(self):
         return self._size
+    
+    def _getnode(self, index):
+        probe = self.head
+        for i in range(index):
+            if probe:
+                probe = probe.next
+            else:
+                raise IndexError("List index out of range")
+        return probe    
+    
+    def __getitem__(self, index):
+        probe = self._getnode(index)
+        if probe:
+            return probe.data
+        else:
+            raise IndexError("List index out of range")
+    
+    def __setitem__(self, index, item):
+        probe = self._getnode(index)
+        if probe:
+            probe.data = item
+        else: 
+            raise IndexError("List index out of range")
+    
+    def index(self, item):
+        probe = self.head
+        i = 0
+        while(probe):
+            if probe.data == item:
+                return i
+            probe = probe.next
+            i+=1
+        raise ValueError("{} is not int list".format(item))
 
     def __repr__(self) -> str:
-        return f'LinkedList(head={self.head}, size={self._size})'
-        """r = ""
+        r = ""
         pointer = self.head
-        while(pointer.next):
-            r = r + str(pointer.data) + " "
+        while(pointer):
+            r = r + str(pointer.data) + "->"         
             pointer = pointer.next
-        return r"""
+        return r
 
     def __str__(self) -> str:
         return self.__repr__()
 
-a = LinkedList()
-a.append(5)
-print(len(a))
-print()
+lista = LinkedList()
+# inserindo na última posição
+lista.append(5)
+lista.append(10) 
+# retorna o tamanho da lista
+print(f'O tamanho da lista é: {lista._size}')   
+# retorna o item baseado no indice
+print(f'O valor no indice 0 é: {lista.__getitem__(0)}')
+print(f'O valor no indice 1 é: {lista.__getitem__(1)}')
+# substitui um item baseado no indice      
+lista[0] = 10
+lista[1] = 20
+# retorna o indice de um valor    
+print(f'O valor 10 está no indice: {lista.index(10)}')
+print(f'O valor 20 está no indice: {lista.index(20)}')
+# imprime a lista
+print(lista)
